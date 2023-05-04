58cad640383eb178668c4d412ff04edc1566a70b
Dan Banfield <dandbanfield@gmail.com>
2023-05-04 18:32:17 +0100

    Add project files.

---
+++ TempNuke/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TempNuke
+{
+    internal class Program
+    {
+        internal static void Main()
+        {
+            Console.Write("TODO: Write the code, ofc.");
+            Console.ReadKey();
+        }
+    }
+}
